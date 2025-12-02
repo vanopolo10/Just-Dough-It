6474bba85881aca8ef8c20c836a851bae9f0dade
vanopolo10 <Vanopolo10@gmail.com>
2025-12-02 17:23:55 +0300

    Script Sort

--- Assets/Scripts/Shop/MoneyManager.cs
+++ Assets/Scripts/Shop/MoneyManager.cs
@@ -5,21 +5,25 @@ public class MoneyManager : MonoBehaviour
 {
     [SerializeField] private int _money;
     [SerializeField] private TextMeshProUGUI _display;
+    
     public int Money => _money;
 
     private void Start()
     {
         UpdDisplay();
     }
-    private void UpdDisplay() 
+    
+    private void UpdDisplay()
     { 
         _display.text = _money.ToString() + "ð";
     }
+    
     public void AddMoney(int amount)
     {
         _money += amount;
         UpdDisplay();
     }
+    
     public bool TrySpendMoney(int amount) 
     {
         if (_money < amount) 
