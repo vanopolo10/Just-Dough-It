cb9d03a853dc98422b6de1dc93d0eca5f4b2bca3
FedorHigh <fedor.semenov.2008@gmail.com>
2025-12-06 12:39:01 +0300

    Reapply "Merge pull request #8 from vanopolo10/CustomerSystem"
    This reverts commit e11c88cd5d14afeb675bde6dbe36dca541c9607d.

---
+++ Assets/Scripts/Shop/NPC/CustomerTextDisplay.cs
@@ -0,0 +1,13 @@
+using System;
+using UnityEngine;
+
+public class CustomerTextDisplay : MonoBehaviour
+{
+    [Serializable]
+    struct TextSet { };
+
+    public void Query(Query query) { }
+    public void Accept() { }
+    public void Decline() { }
+}
+
