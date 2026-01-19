4397ed23a6480289778199b4f1fb1d7be947e986
vanopolo10 <Vanopolo10@gmail.com>
2026-01-19 17:07:09 +0300

    Revert "Wood Fire Power + new NPC models"
    This reverts commit 3f9e11159e31c35e8b700d4226b6cd96f6130b4b.

--- Assets/Scripts/Shop/Oven/Oven.cs
+++ Assets/Scripts/Shop/Oven/Oven.cs
@@ -1,49 +1,14 @@
-using System;
-using System.Collections;
 using UnityEngine;
 
 public class Oven : MonoBehaviour
 {
-    private const int MaxFirePower = 100;
-
-    [SerializeField] private int _woodFullPowerTime = 20;
-    
-    private int _woodPower = 20;
-    
-    public event Action<int> FirePowerChanged;
-    
-    public int FirePower {get; private set;}
-
-    public void AddWood()
+    private void Start()
     {
-        StartCoroutine(FireWood());
-    }
-
-    private IEnumerator FireWood()
-    {
-        for (int i = 0; i < _woodPower; i++)
-        {
-            FirePower = Math.Clamp(FirePower + 1, 0, MaxFirePower);
-            FirePowerChanged?.Invoke(FirePower);
-            yield return new WaitForSeconds(1);
-        }
         
-        StartCoroutine(WaitFullPower());
     }
-    
-    private IEnumerator WaitFullPower()
+
+    private void Update()
     {
-        yield return new WaitForSeconds(_woodFullPowerTime);
-        StartCoroutine(FadeWood());
-    }
-    
-    private IEnumerator FadeWood()
-    {
-        for (int i = 0; i < _woodPower; i++)
-        {
-            FirePower = Math.Clamp(FirePower - 1, 0, MaxFirePower);
-            FirePowerChanged?.Invoke(FirePower);
-            yield return new WaitForSeconds(1);
-        }
+        
     }
 }
