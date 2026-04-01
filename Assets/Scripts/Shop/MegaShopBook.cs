5283a84069b3efd907d1673c8c15ab311abd75a0
vanopolo10 <Vanopolo10@gmail.com>
2026-04-01 19:29:24 +0300

    Делаем

--- Assets/Scripts/Shop/MegaShopBook.cs
+++ Assets/Scripts/Shop/MegaShopBook.cs
@@ -1,7 +1,3 @@
-<<<<<<< HEAD
-=======
-using NUnit.Framework;
->>>>>>> parent of 09d084a (Revert "Merge branch 'ShopRework'")
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -10,13 +6,9 @@ public class MegaShopBook : ShopBook
     [SerializeField] private List<SingularShopBook> _books;
     [SerializeField] private AnimationClip _finalClip;
     [SerializeField] private float _finalClipSpeed = 1, _finalDelay = 0;
-<<<<<<< HEAD
-    
+
     private int _booksLeft;
 
-=======
-    private int _booksLeft;
->>>>>>> parent of 09d084a (Revert "Merge branch 'ShopRework'")
     public override void Start()
     {
         base.Start();
@@ -26,26 +18,16 @@ public class MegaShopBook : ShopBook
 
     public override void OnMovedOutOfPosition()
     {
-<<<<<<< HEAD
         _canvas.SetActive(false);
         Debug.Log($"Megabook moving out of position, from {transform.position} to {_initialPosition}");
 
         foreach (var book in _books)
             book.OnMovedOutOfPosition();
-=======
-        _canvas.SetActive( false );
-        Debug.Log($"Megabook moving out of position, from {transform.position} to {_initialPosition}");
-        for (int i = 0; i < _books.Count; i++)
-        {    
-            _books[i].OnMovedOutOfPosition();
-        }
->>>>>>> parent of 09d084a (Revert "Merge branch 'ShopRework'")
     }
 
     public override void OnMovedToPosition()
     {
         _canvas.SetActive(_bought);
-<<<<<<< HEAD
 
         foreach (var book in _books)
             book.OnMovedToPosition();
@@ -53,15 +35,6 @@ public class MegaShopBook : ShopBook
 
     public void OnChildBookPurchase()
     {
-=======
-        for (int i = 0; i < _books.Count; i++)
-        {
-            _books[i].OnMovedToPosition();
-        }
-    }
-
-    public void OnChildBookPurchase() { 
->>>>>>> parent of 09d084a (Revert "Merge branch 'ShopRework'")
         _booksLeft--;
         Debug.Log("Megabook child purchased. books left: " + _booksLeft);
         if (_booksLeft <= 0)
@@ -71,23 +44,13 @@ public class MegaShopBook : ShopBook
             _canvas.SetActive(true);
             Debug.Log($"Megabook canvas ({_canvas.name}) activity set to {_canvas.activeSelf}");
         }
-<<<<<<< HEAD
     }
 
     public void DisposeOfBook()
     {
         ShopManager shopManager = GetComponentInParent<ShopManager>();
-
         if (shopManager == null)
         {
-=======
-        
-    }
-
-    public void DisposeOfBook() { 
-        ShopManager shopManager = GetComponentInParent<ShopManager>();
-        if (shopManager == null) {
->>>>>>> parent of 09d084a (Revert "Merge branch 'ShopRework'")
             Debug.Log("[MegaShopBook] found no manager to dispose of self");
         }
 
@@ -97,8 +60,4 @@ public class MegaShopBook : ShopBook
 
         shopManager.Invoke(nameof(shopManager.CycleBook), (_finalClip.length) / _finalClipSpeed + _finalDelay);
     }
-<<<<<<< HEAD
 }
-=======
-}
->>>>>>> parent of 09d084a (Revert "Merge branch 'ShopRework'")
