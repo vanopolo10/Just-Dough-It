13de0958a055a5dfbad0ed261328bcba34652f23
Vanopolo <Vanopolo10@gmail.com>
2026-04-09 18:41:36 +0300

    Update SleepButton.cs

--- Assets/Scripts/UI/SleepButton.cs
+++ Assets/Scripts/UI/SleepButton.cs
@@ -27,6 +27,8 @@ public class SleepButton : MonoBehaviour
     
     private void OnEnable()
     {
+        _button = GetComponent<Button>();
+        
         if (_isTutorial && _tutorialRunner != null)
             _tutorialRunner.TutorialComplited += OnCustomersEnded;
         else if (_customerManager != null)
